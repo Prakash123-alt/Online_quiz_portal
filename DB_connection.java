c0593c4b6aa51cc280d71236cd37cf44121f8c54
Chejarla Prakash Rao <prakashraochejarla@gmail.com>
2025-07-11 08:55:59 +0530

    Add files via upload

---
+++ DB_connection.java
@@ -0,0 +1,7 @@
+import java.sql.*;
+
+public class DBConnection {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz_db", "root", "your_password");
+    }
+}
